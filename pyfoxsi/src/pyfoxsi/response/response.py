2c6dee0fe989424d3ae51843f8b991c519a8bb1e
Steven Christe <steven.d.christe@nasa.gov>
2015-08-29 02:59:41 -0400

    pyfoxsi: created new materials object

--- pyfoxsi/src/pyfoxsi/response/response.py
+++ pyfoxsi/src/pyfoxsi/response/response.py
@@ -12,10 +12,12 @@ import os
 
 import matplotlib.pyplot as plt
 import astropy.units as u
-
+from scipy import interpolate
 import pyfoxsi
 
-__all__ = [Response']
+import h5py
+
+__all__ = ['Response', 'Material']
 
 
 class Response(object):
@@ -39,6 +41,7 @@ class Response(object):
         # now add the effective area of all of the shells together
         self.effective_area = pd.DataFrame({'module': self._eff_area_per_shell.sum(axis=1), 'total': self._eff_area_per_shell.sum(axis=1)})
         self.number_of_telescopes = pyfoxsi.number_of_telescopes
+        self.optical_path = []
 
     def plot(self):
         ax = self.effective_area.plot()
@@ -54,3 +57,89 @@ class Response(object):
     def number_of_telescopes(self, x):
         self.effective_area['total'] = self.effective_area['total'] / self.__number_of_telescopes * x
         self.__number_of_telescopes = x
+
+    @property
+    def optical_path(self):
+        return self.__optical_path
+
+    @optical_path.setter
+    def optical_path(self, x):
+        self.effective_area['total'] = self.effective_area['total'] / self.__number_of_telescopes * x
+        self.__number_of_telescopes = x
+
+
+class Material(object):
+    """An object which provides the optical properties of a material in x-rays
+
+    Parameters
+    ----------
+    material : str
+        A string representing a material (e.g. cdte, be, mylar, si)
+    thickness : `astropy.units.Quantity`
+        The thickness of the material in the optical path.
+
+    Examples
+    --------
+    >>> from pyfoxsi.response import Material
+    >>> import astropy.units as u
+    >>> detector = Material('cdte', 500 * u.um)
+    >>> thermal_blankets = Material('mylar', 0.5 * u.mm)
+    """
+
+    def __init__(self, material, thickness):
+        self.material = material
+        self.thickness = thickness
+
+        path = os.path.dirname(pyfoxsi.__file__)
+        for i in np.arange(3):
+            path = os.path.dirname(path)
+        path = os.path.join(path, 'data/')
+        filename = 'mass_attenuation_coefficient.hdf5'
+        data_file = os.path.join(path, filename)
+
+        h = h5py.File(data_file, 'r')
+        data = h[self.material]
+        self._source_data = data
+
+        self.density = u.Quantity(self._source_data.attrs['density'], self._source_data.attrs['density unit'])
+
+        data_energy_kev = np.log10(self._source_data[0,:] * 1000)
+        data_attenuation_coeff = np.log10(self._source_data[1,:])
+        self._f = interpolate.interp1d(data_energy_kev, data_attenuation_coeff, bounds_error=False, fill_value=0.0)
+        self._mass_attenuation_coefficient_func = lambda x: 10 ** self._f(np.log10(x))
+
+    def __repr__(self):
+        """Returns a human-readable representation."""
+        return '<Material ' + str(self.material) + ' ' + str(self.thickness) + '>'
+
+    def transmission(self, energy):
+    	"""Provide the transmission fraction (0 to 1).
+
+        Parameters
+        ----------
+        energy : `astropy.units.Quantity`
+            An array of energies in keV
+        """
+    	coefficients = self._mass_attenuation_coefficient_func(energy) * u.cm ** 2 / u.gram
+    	transmission = np.exp(- coefficients * self.density * self.thickness)
+    	return transmission
+
+    def absorption(self, energy):
+	    """Provides the absorption fraction (0 to 1).
+
+        Parameters
+        ----------
+        energy : `astropy.units.Quantity`
+            An array of energies in keV.
+        """
+	    return 1 - self.transmission(energy)
+
+    def plot(self):
+        f = plt.figure()
+        energies = np.arange(1, 60)
+        plt.plot(energies, self.transmission(energies), label='Transmission')
+        plt.plot(energies, self.absorption(energies), label='Absorption')
+        plt.ylim(0, 1.2)
+        plt.legend()
+        plt.title(self.material + ' ' + str(self.thickness))
+        plt.xlabel('Energy [keV]')
