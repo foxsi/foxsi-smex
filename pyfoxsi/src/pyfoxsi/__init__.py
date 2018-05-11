e97edb73b237c366dcd80a93c8a39f8a1167941e
Steven Christe <steven.d.christe@nasa.gov>
2018-05-10 22:51:49 -0400

    fixes to parameters

--- pyfoxsi/src/pyfoxsi/__init__.py
+++ pyfoxsi/src/pyfoxsi/__init__.py
@@ -2,16 +2,16 @@ from __future__ import absolute_import
 import astropy.units as u
 from datetime import datetime
 
-number_of_telescopes = 2
-mission_title = 'FOXSI SMEX'
+mission_title = 'FOXSI-SMEX'
+launch_date = datetime(2022, 7, 1)
 
-launch_date = datetime(2022, 07, 01)
+number_of_dsi_telescopes = 2
 
-shutter_material = 'aluminum'
-shutters_thickness = [0, 0.02, 0.08, 0.3, 1.3, 6] * u.mm
+shutter_material = 'Al'
+shutter_thickness = [0, 0.02, 0.08, 0.3, 1.3, 6] * u.mm
 detector_material = 'cdte'
 detector_thickness = 1.0 * u.mm
 blanket_material = 'mylar'
 blanket_thickness = 0.5 * u.mm
 
-focal_length = 14 * u.m
+dsi_focal_length = 14 * u.m
